539abc53a930acd83c97d3e906d85071cae892d6
sdist <sdist@itam.mx>
2018-04-20 08:33:25 -0500

    implementacion imports

--- prueba/src/test/java/TestSolu.java
+++ prueba/src/test/java/TestSolu.java
@@ -1,3 +1,10 @@
+import org.apache.log4j.Logger;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
 public class TestSolu{
     solu bla = new solu();
     @Test
